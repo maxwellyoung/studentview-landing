cbc4c626fcebbdf38d0b63423c10788d96025ba2
Maxwell Young <maxtheyoung@gmail.com>
2024-07-23 09:57:32 +1200

    add fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/StudentviewFavicon.svg" sizes="any" />
+        <link rel="icon" href="/favicon.svg" sizes="any" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
