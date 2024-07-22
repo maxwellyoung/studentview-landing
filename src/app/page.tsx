844301ce41a3be53058e043d2a845bf10555b0dd
Maxwell Young <maxtheyoung@gmail.com>
2024-07-23 09:49:32 +1200

    major updates inc email form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useEffect } from "react";
+import Link from "next/link";
+import TwitterIcon from "../components/TwitterIcon";
 
 const LandingPage = () => {
   useEffect(() => {
@@ -7,9 +9,9 @@ const LandingPage = () => {
       const header = document.querySelector("header");
       if (header) {
         if (window.scrollY > 50) {
-          header.classList.add("bg-gray-800", "shadow-md");
+          header.classList.add("bg-white", "shadow-md");
         } else {
-          header.classList.remove("bg-gray-800", "shadow-md");
+          header.classList.remove("bg-white", "shadow-md");
         }
       }
     };
@@ -17,24 +19,44 @@ const LandingPage = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://eomail6.com/form/512efc6e-4872-11ef-be09-b1214129c663.js";
+    script.async = true;
+    script.setAttribute("data-form", "512efc6e-4872-11ef-be09-b1214129c663");
+    const formContainer = document.getElementById("email-octopus-form");
+    formContainer?.appendChild(script);
+
+    return () => {
+      formContainer?.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div className="min-h-screen bg-dark-gray text-white">
-      <header className="fixed w-full top-0 z-50 transition-all duration-300">
+    <div className="min-h-screen bg-white text-black">
+      <header className="fixed w-full top-0 z-50 transition-all duration-300 bg-white">
         <div className="container mx-auto p-6 flex justify-between items-center">
           <h1 className="text-3xl font-bold text-green-500">StudentView</h1>
-          <nav className="flex space-x-6">
+          <nav className="flex items-center space-x-6">
             <a
               href="#features"
-              className="text-lg hover:text-green-600 text-green-500 transition-colors duration-300"
+              className="text-lg hover:text-green-400 text-green-500 transition-colors duration-300"
             >
               Features
             </a>
-            {/* <a
+            <a
               href="#stay-updated"
-              className="text-lg hover:text-green-600 text-green-500 transition-colors duration-300"
+              className="text-lg hover:text-green-400 text-green-500 transition-colors duration-300"
             >
               Stay Updated
-            </a> */}
+            </a>
+            <a
+              href="https://twitter.com/yourprofile"
+              className="flex items-center justify-center hover:text-gray-300"
+            >
+              <TwitterIcon />
+            </a>
           </nav>
         </div>
       </header>
@@ -45,26 +67,26 @@ const LandingPage = () => {
           <br />
           Live Better.
         </h2>
-        <p className="text-2xl md:text-3xl mb-12 max-w-2xl text-gray-400">
+        <p className="text-2xl md:text-3xl mb-12 max-w-2xl text-gray-600">
           The ultimate tool to manage your studies, finances, and well-being.
         </p>
-        {/* <a
+        <a
           href="#stay-updated"
-          className="bg-green-500 text-dark-gray py-4 px-12 rounded-full text-xl font-semibold shadow-lg transition-transform transform hover:scale-105"
+          className="bg-green-500 text-white py-4 px-12 rounded-full text-xl font-semibold shadow-lg transition-transform transform hover:scale-105"
         >
           Stay Updated
-        </a> */}
+        </a>
       </main>
 
-      <section id="features" className="py-32 bg-gray-800 text-gray-200">
+      <section id="features" className="py-32 bg-gray-100 text-gray-800">
         <div className="container mx-auto text-center">
           <h3 className="text-5xl md:text-6xl font-bold mb-16 text-green-500">
             Features
           </h3>
           <div className="flex flex-wrap justify-center gap-8">
             <div className="w-full md:w-1/3 p-8">
-              <div className="p-10 h-full rounded-lg shadow-lg bg-gray-700 hover:shadow-2xl transition-shadow duration-300">
-                <h4 className="text-3xl font-semibold mb-4 text-light-green">
+              <div className="p-10 h-full rounded-lg shadow-lg bg-white hover:shadow-2xl transition-shadow duration-300">
+                <h4 className="text-3xl font-semibold mb-4 text-green-500">
                   Budget Management
                 </h4>
                 <p className="text-lg">
@@ -73,8 +95,8 @@ const LandingPage = () => {
               </div>
             </div>
             <div className="w-full md:w-1/3 p-8">
-              <div className="p-10 h-full rounded-lg shadow-lg bg-gray-700 hover:shadow-2xl transition-shadow duration-300">
-                <h4 className="text-3xl font-semibold mb-4 text-light-green">
+              <div className="p-10 h-full rounded-lg shadow-lg bg-white hover:shadow-2xl transition-shadow duration-300">
+                <h4 className="text-3xl font-semibold mb-4 text-green-500">
                   Study Notes
                 </h4>
                 <p className="text-lg">
@@ -83,8 +105,8 @@ const LandingPage = () => {
               </div>
             </div>
             <div className="w-full md:w-1/3 p-8">
-              <div className="p-10 h-full rounded-lg shadow-lg bg-gray-700 hover:shadow-2xl transition-shadow duration-300">
-                <h4 className="text-3xl font-semibold mb-4 text-light-green">
+              <div className="p-10 h-full rounded-lg shadow-lg bg-white hover:shadow-2xl transition-shadow duration-300">
+                <h4 className="text-3xl font-semibold mb-4 text-green-500">
                   Meditation
                 </h4>
                 <p className="text-lg">
@@ -96,19 +118,19 @@ const LandingPage = () => {
         </div>
       </section>
 
-      <section id="ios-app" className="py-32 bg-gray-900 text-white">
+      <section id="ios-app" className="py-32 bg-gray-50 text-gray-900">
         <div className="container mx-auto text-center">
           <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-500">
             Web and iOS apps coming soon!
           </h3>
-          <p className="text-xl md:text-2xl mb-12 text-gray-300">
+          <p className="text-xl md:text-2xl mb-12 text-gray-600">
             Get ready to take StudentView with you, wherever you go. Stay tuned
             for our launch!
           </p>
         </div>
       </section>
 
-      {/* <section id="stay-updated" className="py-32 bg-gray-800 text-gray-200">
+      <section id="stay-updated" className="py-32 bg-white text-gray-800">
         <div className="container mx-auto text-center">
           <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-500">
             Stay Updated!
@@ -117,29 +139,19 @@ const LandingPage = () => {
             Sign up for our newsletter to get the latest updates and be the
             first to know when we launch.
           </p>
-          <form
-            action="https://emailoctopus.com/lists40ead1b4-4815-11ef-bce6-8f905a46a786/members/embedded/1.1/add"
-            method="post"
-            className="flex flex-col items-center"
-          >
-            <input
-              type="email"
-              name="email_address"
-              placeholder="Enter your email"
-              className="mb-4 px-4 py-2 rounded-md border border-gray-600 w-80 bg-gray-700 text-white"
-              required
-            />
-            <button
-              type="submit"
-              className="bg-green-500 text-dark-gray py-3 px-8 rounded-full text-lg font-semibold shadow-lg transition-transform transform hover:scale-105"
-            >
-              Subscribe
-            </button>
-          </form>
+          <div id="email-octopus-form" className="flex justify-center"></div>
         </div>
-      </section> */}
+      </section>
 
       <footer className="py-8 bg-gray-900 text-gray-400 text-center">
+        <div className="flex justify-center space-x-6 mb-4">
+          <a
+            href="https://twitter.com/yourprofile"
+            className="hover:text-gray-300"
+          >
+            <TwitterIcon />
+          </a>
+        </div>
         <p>© 2024 StudentView. All rights reserved.</p>
         <p>
           Designed by{" "}
