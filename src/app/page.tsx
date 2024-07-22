c9fd27fb20132b8f5a8fbc423851c19cfb91159f
Maxwell Young <maxtheyoung@gmail.com>
2024-07-22 23:29:55 +1200

    version 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,158 @@
-import Image from "next/image";
+"use client";
+import { useEffect } from "react";
 
-export default function Home() {
+const LandingPage = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector("header");
+      if (header) {
+        if (window.scrollY > 50) {
+          header.classList.add("bg-gray-800", "shadow-md");
+        } else {
+          header.classList.remove("bg-gray-800", "shadow-md");
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen bg-dark-gray text-white">
+      <header className="fixed w-full top-0 z-50 transition-all duration-300">
+        <div className="container mx-auto p-6 flex justify-between items-center">
+          <h1 className="text-3xl font-bold text-green-500">StudentView</h1>
+          <nav className="flex space-x-6">
+            <a
+              href="#features"
+              className="text-lg hover:text-green-600 text-green-500 transition-colors duration-300"
+            >
+              Features
+            </a>
+            {/* <a
+              href="#stay-updated"
+              className="text-lg hover:text-green-600 text-green-500 transition-colors duration-300"
+            >
+              Stay Updated
+            </a> */}
+          </nav>
         </div>
-      </div>
+      </header>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <main className="flex flex-col items-center justify-center text-center px-4 py-40 min-h-screen">
+        <h2 className="text-7xl md:text-9xl font-extrabold mb-6 leading-tight text-green-500">
+          Study Smart,
+          <br />
+          Live Better.
+        </h2>
+        <p className="text-2xl md:text-3xl mb-12 max-w-2xl text-gray-400">
+          The ultimate tool to manage your studies, finances, and well-being.
+        </p>
+        {/* <a
+          href="#stay-updated"
+          className="bg-green-500 text-dark-gray py-4 px-12 rounded-full text-xl font-semibold shadow-lg transition-transform transform hover:scale-105"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Stay Updated
+        </a> */}
+      </main>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section id="features" className="py-32 bg-gray-800 text-gray-200">
+        <div className="container mx-auto text-center">
+          <h3 className="text-5xl md:text-6xl font-bold mb-16 text-green-500">
+            Features
+          </h3>
+          <div className="flex flex-wrap justify-center gap-8">
+            <div className="w-full md:w-1/3 p-8">
+              <div className="p-10 h-full rounded-lg shadow-lg bg-gray-700 hover:shadow-2xl transition-shadow duration-300">
+                <h4 className="text-3xl font-semibold mb-4 text-light-green">
+                  Budget Management
+                </h4>
+                <p className="text-lg">
+                  Track your expenses and manage your budget like a pro.
+                </p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/3 p-8">
+              <div className="p-10 h-full rounded-lg shadow-lg bg-gray-700 hover:shadow-2xl transition-shadow duration-300">
+                <h4 className="text-3xl font-semibold mb-4 text-light-green">
+                  Study Notes
+                </h4>
+                <p className="text-lg">
+                  Keep all your study notes in one organized place.
+                </p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/3 p-8">
+              <div className="p-10 h-full rounded-lg shadow-lg bg-gray-700 hover:shadow-2xl transition-shadow duration-300">
+                <h4 className="text-3xl font-semibold mb-4 text-light-green">
+                  Meditation
+                </h4>
+                <p className="text-lg">
+                  Take a break, meditate, and boost your focus.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+      <section id="ios-app" className="py-32 bg-gray-900 text-white">
+        <div className="container mx-auto text-center">
+          <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-500">
+            Web and iOS Apps Coming Soon!
+          </h3>
+          <p className="text-xl md:text-2xl mb-12 text-gray-300">
+            Get ready to take StudentView with you, wherever you go. Stay tuned
+            for our launch!
           </p>
-        </a>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      {/* <section id="stay-updated" className="py-32 bg-gray-800 text-gray-200">
+        <div className="container mx-auto text-center">
+          <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-500">
+            Stay Updated!
+          </h3>
+          <p className="text-xl md:text-2xl mb-12">
+            Sign up for our newsletter to get the latest updates and be the
+            first to know when we launch.
           </p>
-        </a>
-      </div>
-    </main>
+          <form
+            action="https://emailoctopus.com/lists40ead1b4-4815-11ef-bce6-8f905a46a786/members/embedded/1.1/add"
+            method="post"
+            className="flex flex-col items-center"
+          >
+            <input
+              type="email"
+              name="email_address"
+              placeholder="Enter your email"
+              className="mb-4 px-4 py-2 rounded-md border border-gray-600 w-80 bg-gray-700 text-white"
+              required
+            />
+            <button
+              type="submit"
+              className="bg-green-500 text-dark-gray py-3 px-8 rounded-full text-lg font-semibold shadow-lg transition-transform transform hover:scale-105"
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </section> */}
+
+      <footer className="py-8 bg-gray-900 text-gray-400 text-center">
+        <p>© 2024 StudentView. All rights reserved.</p>
+        <p>
+          Designed by{" "}
+          <a
+            href="https://ninetynine.digital"
+            className="underline hover:text-gray-300 transition-colors duration-300"
+          >
+            ninetynine digital
+          </a>
+        </p>
+      </footer>
+    </div>
   );
-}
+};
+
+export default LandingPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,47 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "gray-100": "#f7fafc",
+        "gray-200": "#edf2f7",
+        "gray-300": "#e2e8f0",
+        "gray-800": "#2d3748",
+        "gray-900": "#1a202c",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
