f5b77ba4f88f3f8c251c1d9715aaae31a2c0c0d7
Maxwell Young <maxtheyoung@gmail.com>
2024-08-07 11:54:17 +1200

    accidentally broke page last commit oops

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,32 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.svg" sizes="any" />
+        <meta
+          name="description"
+          content="Companion app for managing student life developed by ninetynine digital."
+        />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="StudentView" />
+        <meta
+          property="og:description"
+          content="Companion app for managing student life developed by ninetynine digital."
+        />
+        <meta property="og:image" content="./public/preview.jpeg" />
+        <meta property="og:url" content="https://studentview.app" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:title" content="StudentView" />
+        <meta
+          name="twitter:description"
+          content="Companion app for managing student life developed by ninetynine digital."
+        />
+        <meta
+          name="twitter:image"
+          content="https://yourdomain.com/path/to/preview-image.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </head>
       <body className={`${inter.className}`}>{children}</body>
     </html>
