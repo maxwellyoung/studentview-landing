bab9344add5ca2a5d12675ae3b00f281a6bd27af
Maxwell Young <maxtheyoung@gmail.com>
2024-08-09 22:15:53 +1200

    animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
-import favicon from "../../public/favicon.svg"; // Update the path as needed
+import logo from "../../public/logo.png";
 
 const LandingPage = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const controls = useAnimation();
+  const featuresRef = useRef(null);
+  const appsRef = useRef(null);
+  const faqsRef = useRef(null);
+  const noteRef = useRef(null);
+
+  const featuresInView = useInView(featuresRef, { once: true });
+  const appsInView = useInView(appsRef, { once: true });
+  const faqsInView = useInView(faqsRef, { once: true });
+  const noteInView = useInView(noteRef, { once: true });
+
+  useEffect(() => {
+    if (featuresInView) {
+      controls.start("show");
+    }
+  }, [controls, featuresInView]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,10 +46,13 @@ const LandingPage = () => {
     script.async = true;
     script.setAttribute("data-form", "512efc6e-4872-11ef-be09-b1214129c663");
     const formContainer = document.getElementById("email-octopus-form");
-    formContainer?.appendChild(script);
-
+    if (formContainer) {
+      formContainer.appendChild(script);
+    }
     return () => {
-      formContainer?.removeChild(script);
+      if (formContainer) {
+        formContainer.removeChild(script);
+      }
     };
   }, []);
 
@@ -41,29 +61,22 @@ const LandingPage = () => {
       <header className="fixed w-full top-0 z-50 transition-all duration-300 backdrop-blur-md bg-opacity-70 shadow-sm">
         <div className="container mx-auto p-6 flex justify-between items-center">
           <div className="flex items-center space-x-3">
-            <Image
-              src={favicon}
-              alt="StudentView Logo"
-              width={40}
-              height={40}
-            />
+            <Image src={logo} alt="StudentView Logo" width={40} height={40} />
             <h1 className="text-3xl font-semibold tracking-tight text-green-600">
               StudentView
             </h1>
           </div>
           <nav className="hidden md:flex items-center space-x-6">
-            <a
-              href="#features"
-              className="text-lg hover:text-green-400 text-green-600 transition-colors duration-300"
-            >
-              Features
-            </a>
-            <a
-              href="#stay-updated"
-              className="text-lg hover:text-green-400 text-green-600 transition-colors duration-300"
-            >
-              Stay Updated
-            </a>
+            <Link href="#features" legacyBehavior>
+              <a className="text-lg hover:text-green-400 text-green-600 transition-colors duration-300">
+                Features
+              </a>
+            </Link>
+            <Link href="#stay-updated" legacyBehavior>
+              <a className="text-lg hover:text-green-400 text-green-600 transition-colors duration-300">
+                Stay Updated
+              </a>
+            </Link>
           </nav>
           <button
             className="md:hidden text-green-600"
@@ -104,51 +117,88 @@ const LandingPage = () => {
         </div>
         {isMobileMenuOpen && (
           <div className="md:hidden bg-green-50 shadow-md">
-            <a
-              href="#features"
-              className="block px-4 py-2 text-lg text-green-600 hover:bg-green-100"
-              onClick={() => setIsMobileMenuOpen(false)}
-            >
-              Features
-            </a>
-            <a
-              href="#stay-updated"
-              className="block px-4 py-2 text-lg text-green-600 hover:bg-green-100"
-              onClick={() => setIsMobileMenuOpen(false)}
-            >
-              Stay Updated
-            </a>
+            <Link href="#features" legacyBehavior>
+              <a
+                className="block px-4 py-2 text-lg text-green-600 hover:bg-green-100"
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                Features
+              </a>
+            </Link>
+            <Link href="#stay-updated" legacyBehavior>
+              <a
+                className="block px-4 py-2 text-lg text-green-600 hover:bg-green-100"
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                Stay Updated
+              </a>
+            </Link>
           </div>
         )}
       </header>
 
-      <main className="flex flex-col items-center justify-center text-center px-4 py-40 min-h-screen bg-gradient-to-b from-green-50 to-green-100">
-        <h2 className="text-7xl md:text-9xl font-extrabold mb-6 leading-tight text-green-600 tracking-tight">
+      <motion.main
+        className="flex flex-col items-center justify-center text-center px-4 py-40 min-h-screen bg-gradient-to-b from-green-50 to-green-100"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <motion.h2
+          className="text-7xl md:text-9xl font-extrabold mb-6 leading-tight text-green-600 tracking-tight"
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 60 }}
+        >
           Study{" "}
-          <mark className="bg-transparent font-extrabold text-green-600 shadow-[inset_0_-0.5em_0_0_#B8F3FF]">
+          <motion.span
+            className="bg-transparent font-extrabold text-green-600 shadow-[inset_0_-0.5em_0_0_#B8F3FF]"
+            initial={{ scale: 0.8 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.8, ease: "easeInOut" }}
+          >
             Smart
-          </mark>
+          </motion.span>
           ,<br />
           Live Better.
-        </h2>
-        <p className="text-2xl md:text-3xl mb-12 max-w-2xl text-gray-700">
+        </motion.h2>
+        <motion.p
+          className="text-2xl md:text-3xl mb-12 max-w-2xl text-gray-700"
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
           The ultimate tool to manage your studies and well-being.
-        </p>
-        <a
+        </motion.p>
+        <motion.a
           href="#stay-updated"
           className="bg-green-600 text-white py-4 px-12 rounded-full text-xl font-semibold shadow-lg transition-transform transform hover:scale-105"
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 300 }}
         >
           Stay Updated
-        </a>
-      </main>
+        </motion.a>
+      </motion.main>
 
-      <section id="features" className="py-32 bg-white text-gray-800">
-        <div className="container mx-auto text-center">
+      <section
+        id="features"
+        className="py-32 bg-white text-gray-800"
+        ref={featuresRef}
+      >
+        <motion.div
+          className="container mx-auto text-center"
+          initial={{ opacity: 0, y: 50 }}
+          animate={featuresInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ type: "spring", stiffness: 80 }}
+        >
           <h3 className="text-5xl md:text-6xl font-bold mb-16 text-green-600">
             Ease your life with
           </h3>
           <div className="flex flex-wrap justify-center gap-8">
-            <div className="w-full md:w-1/3 p-8">
+            <motion.div
+              className="w-full md:w-1/3 p-8"
+              whileHover={{ scale: 1.05 }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
               <div className="p-10 h-full rounded-lg shadow-lg bg-white hover:shadow-2xl transition-shadow duration-300">
                 <h4 className="text-3xl font-semibold mb-4 text-green-600">
                   Study Notes
@@ -157,8 +207,12 @@ const LandingPage = () => {
                   Keep all your study notes in one organized place.
                 </p>
               </div>
-            </div>
-            <div className="w-full md:w-1/3 p-8">
+            </motion.div>
+            <motion.div
+              className="w-full md:w-1/3 p-8"
+              whileHover={{ scale: 1.05 }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
               <div className="p-10 h-full rounded-lg shadow-lg bg-white hover:shadow-2xl transition-shadow duration-300">
                 <h4 className="text-3xl font-semibold mb-4 text-green-600">
                   Meditation
@@ -167,8 +221,12 @@ const LandingPage = () => {
                   Take a break, meditate, and boost your focus.
                 </p>
               </div>
-            </div>
-            <div className="w-full md:w-1/3 p-8">
+            </motion.div>
+            <motion.div
+              className="w-full md:w-1/3 p-8"
+              whileHover={{ scale: 1.05 }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
               <div className="p-10 h-full rounded-lg shadow-lg bg-white hover:shadow-2xl transition-shadow duration-300">
                 <h4 className="text-3xl font-semibold mb-4 text-green-600">
                   Task Management
@@ -177,36 +235,64 @@ const LandingPage = () => {
                   Organize your tasks and manage your time effectively.
                 </p>
               </div>
-            </div>
+            </motion.div>
           </div>
-        </div>
+        </motion.div>
       </section>
 
-      <section
+      <motion.section
         id="apps"
         className="py-32 bg-gradient-to-b from-white to-green-50 text-gray-900"
+        initial={{ opacity: 0, y: 50 }}
+        animate={appsInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ type: "spring", stiffness: 80 }}
+        ref={appsRef}
       >
         <div className="container mx-auto text-center">
-          <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-600">
+          <motion.h3
+            className="text-4xl md:text-5xl font-bold mb-6 text-green-600"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
             Web and iOS apps coming soon!
-          </h3>
-          <p className="text-xl md:text-2xl mb-12 text-gray-700">
+          </motion.h3>
+          <motion.p
+            className="text-xl md:text-2xl mb-12 text-gray-700"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.7 }}
+          >
             Get ready to take StudentView with you, wherever you go. Stay tuned
             for our launch!
-          </p>
+          </motion.p>
         </div>
-      </section>
+      </motion.section>
 
-      <section
+      <motion.section
         id="faqs"
         className="py-32 bg-gradient-to-b from-green-50 to-white text-gray-800"
+        initial={{ opacity: 0, y: 50 }}
+        animate={faqsInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ type: "spring", stiffness: 80 }}
+        ref={faqsRef}
       >
         <div className="container mx-auto text-center">
-          <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-600">
+          <motion.h3
+            className="text-4xl md:text-5xl font-bold mb-6 text-green-600"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
             FAQs
-          </h3>
-          <div className="text-left max-w-3xl mx-auto">
-            <div className="mb-4">
+          </motion.h3>
+          <motion.div
+            className="text-left max-w-3xl mx-auto"
+            initial={{ opacity: 0 }}
+            animate={faqsInView ? { opacity: 1 } : {}}
+            transition={{ duration: 0.8, staggerChildren: 0.1 }}
+          >
+            <motion.div className="mb-4">
               <button className="w-full text-left text-lg font-semibold text-green-600">
                 What is StudentView?
               </button>
@@ -216,8 +302,8 @@ const LandingPage = () => {
                   studies, tasks, and well-being in one place.
                 </p>
               </div>
-            </div>
-            <div className="mb-4">
+            </motion.div>
+            <motion.div className="mb-4">
               <button className="w-full text-left text-lg font-semibold text-green-600">
                 How can I get the app?
               </button>
@@ -227,8 +313,8 @@ const LandingPage = () => {
                   newsletter.
                 </p>
               </div>
-            </div>
-            <div className="mb-4">
+            </motion.div>
+            <motion.div className="mb-4">
               <button className="w-full text-left text-lg font-semibold text-green-600">
                 Is StudentView free?
               </button>
@@ -238,50 +324,79 @@ const LandingPage = () => {
                   premium features available.
                 </p>
               </div>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </div>
-      </section>
+      </motion.section>
 
-      <section id="stay-updated" className="py-32 bg-white text-gray-800">
-        <div className="container mx-auto text-center">
-          <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-600">
-            Ready to transform your student life?
-          </h3>
-          <div id="email-octopus-form" className="flex justify-center "></div>
-        </div>
-      </section>
-
-      <section
-        id="founder-note"
-        className="py-32 bg-gradient-to-b from-green-50 to-white text-gray-800"
+      <motion.section
+        id="stay-updated"
+        className="py-32 bg-white text-gray-800"
+        initial={{ opacity: 0, y: 50 }}
+        animate={noteInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ type: "spring", stiffness: 80 }}
+        ref={noteRef}
       >
         <div className="container mx-auto text-center">
-          <h3 className="text-4xl md:text-5xl font-bold mb-6 text-green-600">
+          <motion.h3
+            className="text-4xl md:text-5xl font-bold mb-6 text-green-600"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            Ready to transform your student life?
+          </motion.h3>
+          <div id="email-octopus-form" className="flex justify-center "></div>
+        </div>
+      </motion.section>
+
+      <motion.section
+        id="founder-note"
+        className="py-32 bg-gradient-to-b from-green-50 to-white text-gray-800"
+        initial={{ opacity: 0, y: 50 }}
+        animate={noteInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ type: "spring", stiffness: 80 }}
+      >
+        <div className="container mx-auto text-center">
+          <motion.h3
+            className="text-4xl md:text-5xl font-bold mb-6 text-green-600"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
             A Note from Our Founder
-          </h3>
-          <p className="text-xl md:text-2xl mb-12 text-gray-700">
+          </motion.h3>
+          <motion.p
+            className="text-xl md:text-2xl mb-12 text-gray-700"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.7 }}
+          >
             &ldquo;As a student, I face numerous challenges in managing my tasks
             and well-being. That&apos;s why I created StudentView, an app that
             brings everything together in one place to help you live a more
             organized and stress-free student life.&rdquo;
-          </p>
-          <p className="text-lg text-gray-800">
+          </motion.p>
+          <motion.p
+            className="text-lg text-gray-800"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.8 }}
+          >
             - Maxwell Young, Founder &amp; CEO
-          </p>
+          </motion.p>
         </div>
-      </section>
+      </motion.section>
 
       <footer className="py-8 bg-white text-gray-400 text-center">
         <p>© 2024 StudentView. All rights reserved.</p>
         <p>
           Designed by{" "}
-          <a
-            href="https://ninetynine.digital"
-            className="underline hover:text-gray-300 transition-colors duration-300"
-          >
-            ninetynine digital
-          </a>
+          <Link href="https://ninetynine.digital" legacyBehavior>
+            <a className="underline hover:text-gray-300 transition-colors duration-300">
+              ninetynine digital
+            </a>
+          </Link>
         </p>
       </footer>
     </div>
